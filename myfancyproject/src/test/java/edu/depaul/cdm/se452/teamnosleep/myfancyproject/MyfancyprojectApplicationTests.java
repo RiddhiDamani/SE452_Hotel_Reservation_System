4691c164d25fdee390661d403dbfa0f55418d0a6
RiddhiDamani <riddhidamani90@gmail.com>
2021-04-07 16:24:32 -0500

    Testing

---
+++ myfancyproject/src/test/java/edu/depaul/cdm/se452/teamnosleep/myfancyproject/MyfancyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.depaul.cdm.se452.teamnosleep.myfancyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfancyprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
